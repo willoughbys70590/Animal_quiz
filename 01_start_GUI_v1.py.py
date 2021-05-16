ce40ea02aee0552ae126c1a01b396c45eaa618d5
willoughbys70590 <60673309+willoughbys70590@users.noreply.github.com>
2021-05-17 09:42:22 +1200

    the latest code start GUI partly working and qustion generator working

--- 01_start_GUI_v1.py.py
+++ 01_start_GUI_v1.py.py
@@ -2,22 +2,43 @@ from tkinter import *
 from functools import partial  # To prevent unwanted windows
 import random
 
-
+# The start function is to show the title of the quiz little instructions  and a start button
 class Start:
-    def __init__(self, prevent):
+    def __init__(self, prevent,):
         # Make the starting frame and how wide and height is going to be
-        self.start_frame = Frame(padx=90, pady=230)
+        self.start_frame = Frame(padx=10, pady=10)
         self.start_frame.grid()
 
         # Animal quiz heading, make sure its in the center,text size is readerable and colour
+        # Heading in row 0
         self.Animal_quiz_label = Label(self.start_frame,
                                        text="Animal quiz",
-                                       font="Arial 19 bold")
+                                       font="Arial 25 bold")
         self.Animal_quiz_label.grid(row=0)
 
-        
+        # The first set of instructions that you will need to follow
+        # Sub-title in row 1
+        self.Animal_instrustions = Label(self.start_frame, font="Arial 15 italic",
+                                        text="To start the quiz press "
+                                        " the start button to start.",
+                                        wrap=275, justify=LEFT, padx=10, pady=10)
+        self.Animal_instrustions.grid(row=1)
 
+        # Button font goes here
+        button_font = "Arial 17 bold"
 
+        # The start button to start the questions
+        self.start_quiz_button = Button(self.start_frame, text="start",
+                                command=self.to_quiz,
+                            font=button_font, bg="#0000FF")
+        self.start_quiz_button.grid(row=4,column=0,pady=10)
+
+        self.start_quiz_button.config(state=NORMAL)
+
+    def to_quiz(self):
+        get_quiz = Quiz(self)
+
+       
 # main routine
 if __name__ == "__main__":
     root = Tk()
--- 02_quesiton_maker.py
+++ 02_quesiton_maker.py
@@ -1,4 +1,5 @@
-import random 
+# code to import the list and make sure it only says the Adult name not the baby name also
+import random
 
 import csv
 
@@ -6,10 +7,11 @@ with open('Animal_list.csv', newline='') as f:
     reader = csv.reader(f)
     Animal_list = list(reader)
 
-pair = random.choice(Animal_list)
-
-question = "what is the name of a baby? {}".format(pair)
+Animal= random.choice(Animal_list)
+adult = Animal[0]
+question = "what is the name of a baby? {}".format(adult)
 print(question)
+
 
 
 
